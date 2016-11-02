29f479b232519b42b352f60c54bb60045df83048
Charles De Barros <charlesdebarros@yahoo.co.uk>
2016-11-02 20:09:14 +0000

    Adding public crawl method.

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -31,4 +31,46 @@ class WebCrawler
     urls = active_urls.split
     return urls
   end
+
+  public
+
+  def crawl
+    links_found = 0
+    agent = Mechanize.new
+
+    agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    fetched_urls = fetch_database_urls
+    fetched_urls.each do |url_to_crawl|
+      begin
+      page = agent.get(url_to_crawl)
+      links = page.links
+      links.each do |link|
+        scraped_url = link.attributes['href']
+        next if scraped_url == '#'
+        case scraped_url[0..4]
+        when 'https' || 'http:' || 'ftp:/'
+          save_site_crawl(scraped_url)
+          puts "Chacked: #{scraped_url}\n--------------------\n"
+        else
+          url_split = url_to_crawl.split('/')
+          if scraped_url[0] == '/'
+            final_url = url_split[0] + '//' + url_split[2] + scraped_url
+          else
+            final_url = url_split[0] + '//' + url_split[2] + '/' + scraped_url
+          end
+          save_site_crawl(final_url)
+          puts "Checked: #{final_url}\n--------------------\n"
+        end
+        links_found += 1
+      end
+    rescue StandardError => get_message
+        puts "Request Level Error: #{get_message}"
+      end
+    end
+    puts "Status update: #{links_found} links found."
+  end
 end
+
+crawler = WebCrawler.new('./lib/urls.txt')
+crawler.crawl
