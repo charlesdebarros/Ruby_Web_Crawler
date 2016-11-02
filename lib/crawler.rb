481c2cdc9a7cd26c0e4e227d6381172b42bcfc55
Charles De Barros <charlesdebarros@yahoo.co.uk>
2016-11-02 10:56:36 +0000

    Adding private method save_site_crawl

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -5,4 +5,18 @@ class WebCrawler
   def initialize(file_name)
     @file = file_name
   end
+
+  private
+
+  def save_site_crawl(site_url)
+    begin
+      if check(site_url)
+        File.open(@file, 'a') do |data|
+          data.puts site_url
+        end
+      end
+    rescue StandardError => error_message
+      puts "ERROR: #{error_message}"
+    end
+  end
 end
